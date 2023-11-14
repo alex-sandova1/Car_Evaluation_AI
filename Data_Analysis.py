b7e405e5803df1e2850d8dfd8790753521c4ee8d
alex-sandova1 <alex3sandova@gmail.com>
2023-11-13 22:39:56 -0800

    worked on the ruled based system

--- Data_Analysis.py
+++ Data_Analysis.py
@@ -1,45 +1,41 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
-# Read the data
+
+#check for null values in data
 data = pd.read_csv('car.data', names=['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class'])
+print(data.isnull().sum())
 
-# Prepare the data
-X = data[['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety']]
-y = data['class']
+#check for 0 values in data
+print((data[['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class']] == 0).sum())
 
-#split into training, validation and test data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25)
-
-#convert string data to numerical data
-X_train = pd.get_dummies(X_train)
-X_test = pd.get_dummies(X_test)
-X_val = pd.get_dummies(X_val)
-
-#store cars by number of doors
-two_doors = data[data['doors'] == '2']
-three_doors = data[data['doors'] == '3']
-four_doors = data[data['doors'] == '4']
-five_or_more_doors = data[data['doors'] == '5more']
-
-#print cars by number of persons
-print("Number of cars with 2 doors: " + str(len(two_doors)))
-print("Number of cars with 3 doors: " + str(len(three_doors)))
-print("Number of cars with 4 doors: " + str(len(four_doors)))
-print("Number of cars with 5 or more doors: " + str(len(five_or_more_doors)))
-
-#storew caers by number of persons
-two_persons = data[data['persons'] == '2']
-four_persons = data[data['persons'] == '4']
-more_than_four_persons = data[data['persons'] == 'more']
-
-#print cars by number of persons
-print("Number of cars with 2 persons: " + str(len(two_persons)))
-print("Number of cars with 4 persons: " + str(len(four_persons)))
-print("Number of cars with more than 4 persons: " + str(len(more_than_four_persons)))
-
-#
-
-
-
+#while loop to go through data and check for bad deals
+i = 0
+while i < len(data):
+    if data['buying'][i] == 'vhigh': 
+        if data['maint'][i] == 'vhigh' or data['maint'][i] == 'high':
+            print("car #", [i], " is a bad deal")
+        elif data['maint'][i] == 'med' and data['safety'][i] == 'high':
+            print("car #", [i], " is an acceptable deal")
+        elif data['maint'][i] == 'low' and data['safety'][i] == 'high' and data['lug_boot'][i] == 'big':
+            print("car #", [i], " is an acceptable deal")
+        elif data['maint'][i] == 'low' and data['safety'][i] == 'med' and (data['lug_boot'][i] == 'med' or data['lug_boot'][i] == 'big') and (data['doors'][i] == '2' or data['doors'][i] == '3' or data['doors'][i] == '4'):
+            print("car #", [i], " is an acceptable deal")
+    elif data['buying'][i] == 'high':
+        if data['maint'][i] == 'vhigh':
+            print("car #", [i], " is a bad deal")
+        elif data['maint'][i] == 'high':
+            if data['safety'][i] == 'high' and data['persons'][i] == '4':
+                print("car #", [i], " is an acceptable deal")
+            elif data['safety'][i] == 'high' and data['persons'][i] == 'more':
+                print("car #", [i], " is an acceptable deal")
+            elif data['safety'][i] == 'med' and (data['lug_boot'][i] == 'big' or (data['lug_boot'][i] == 'med' and data['person'][i] == 'more')):
+                print("car #", [i], " is an acceptable deal")
+        elif data['buying'][i] ==  'med':
+            
+            
+            
+        
+        
+    else:
+        print("car #", [i], " is a bad deal")
+    i = i + 1
